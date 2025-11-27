2e92774b7f42e7c14f99ea4287018e4639f391bd
Ashu Chaudhary <ashuchaudhary@Ashus-MacBook-Air.local>
2025-11-27 10:16:24 +0530

    Practical 7: Updated print message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello from Practical 7 by Zorawar!";
 
     public App() {}
 
